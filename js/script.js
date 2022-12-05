e2144ef97c0d809dc758b7b17c7b2d3bc5a5aba1
MatteoCarbone17 <matteocarbone17@live.com>
2022-12-05 17:11:28 +0100

    <br> it's not a fix

--- js/script.js
+++ js/script.js
@@ -13,34 +13,34 @@ Stampare le stesse informazioni su DOM sottoforma di stringhe
 
 const listTeam = [
    {
-        name :  'Wayne Barnett   ',
-        role :'Founder & CEO',
-        picture : 'wayne-barnett-founder-ceo.jpg',
+        name :  'Wayne Barnett <br>',
+        role :'Founder & CEO <br>',
+        picture : 'wayne-barnett-founder-ceo.jpg <br>',
     },
     {
-        name : 'Walter Gordon   ',
-        role : 'Office Manager ',
-        picture :'walter-gordon-office-manager.jpg', 
+        name : 'Walter Gordon   <br>',
+        role : 'Office Manager <br>',
+        picture :'walter-gordon-office-manager.jpg <br>', 
     },
     {
-        name : 'Angela Caroll ',
-        role : 'Chief Editor ',
-        picture : 'angela-caroll-chief-editor.jpg',
+        name : 'Angela Caroll <br>',
+        role : 'Chief Editor <br>',
+        picture : 'angela-caroll-chief-editor.jpg <br>',
     },
     {
-        name : 'Angela Lopez ',
-        role :'Social Media',
-        picture : 'Manager  angela-lopez-social-media-manager.jpg',
+        name : 'Angela Lopez <br>',
+        role :'Social Media <br>',
+        picture : 'Manager  angela-lopez-social-media-manager.jpg <br>',
     },
     {
-        name : 'Scott Estrada   ',
-        role :'Developer  ',
-        picture : 'scott-estrada-developer.jpg',
+        name : 'Scott Estrada  <br> ',
+        role :'Developer  <br>',
+        picture : 'scott-estrada-developer.jpg <br>',
     },
     {
-        name : 'Barbara Ramos   ',
-        role :'Graphic Designer',
-        picture : 'barbara-ramos-graphic-designer.jpg',
+        name : 'Barbara Ramos  <br> ',
+        role :'Graphic Designer <br>',
+        picture : 'barbara-ramos-graphic-designer.jpg <br>',
     },
 ];
  
